b41d336f222086f9e1ccc79ab8e2b1dd76d7f58e
smartcoding51 <keinakano415@gmail.com>
2023-07-17 04:15:02 +0900

    add env example file & update hardhat configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,41 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: '0.8.19',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      }
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      forking: {
+        enabled: true,
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_MAINNET}`,
+      }
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_GOERLI}`,
+      chainId: 5,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY_MAINNET}`,
+      chainId: 1,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  gasReporter: {
+    enabled: process.env.GAS_REPORT !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
 };
